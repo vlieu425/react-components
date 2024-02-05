977fd2406ff55bb9df2d90f6bd0e3815ef7ddfad
vlieu425 <121201991+vlieu425@users.noreply.github.com>
2024-02-05 10:19:40 -0800

    create unordered grocery list and render to DOM using react

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // TODO
+const App = function () {
+  return (
+    <ul>
+      <li> item 1</li>
+      <li> item 2</li>
+    </ul>
+  )
+
+}
+
+
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById(
+    'app'
+  )
+);
+
