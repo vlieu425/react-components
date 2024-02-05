35cadddada21fe67ea1f40a3b65a703c39c714bb
vlieu425 <121201991+vlieu425@users.noreply.github.com>
2024-02-05 11:28:56 -0800

    dynamically render list elements and add click listener per list element

--- app.js
+++ app.js
@@ -1,27 +1,63 @@
 // TODO
+
+
 const App = function () {
+  // const [groceries, setGroceries] = useState(['Cucumber', 'Kale'])
+  const groceries = ['Cucumber', 'Kale']
+
   return (
     <div>
-      <h2>My Todo list</h2>
-      <Cucumbers/>
-      <Kale/>
+      <h2>My Grocery List</h2>
+      <ul>
+        {/* JSX expects array of elements!! */}
+        {groceries.map((item) => {
+          return <GroceryListItem item = {item}/>
+          })
+        }
+
+      </ul>
     </div>
   )
-
 }
 
 
-const Cucumbers = function () {
+const GroceryListItem = function (props) {
+  const onListItemClick = (event) => {
+    console.log(`You clicked ${event.target.textContent}!`);
+  }
   return (
-    <div>Cucumbers</div>
+    <div>
+      <li onClick = {onListItemClick}>{props.item}</li>
+    </div>
   )
 }
 
-const Kale = function () {
-  return (
-    <div>Kale</div>
-  )
-}
+
+
+
+// const App = function () {
+//   return (
+//     <div>
+//       <h2>My Todo list</h2>
+//       <Cucumbers/>
+//       <Kale/>
+//     </div>
+//   )
+
+// }
+
+
+// const Cucumbers = function () {
+//   return (
+//     <div>Cucumbers</div>
+//   )
+// }
+
+// const Kale = function () {
+//   return (
+//     <div>Kale</div>
+//   )
+// }
 
 
 
